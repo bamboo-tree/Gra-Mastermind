ef0bad415261ad7d6dbdadba64279fe2ce309669
Maciej Kamiński <maciej.kam03@gmail.com>
2023-05-06 10:56:53 +0200

    Zmienione grafiki, na bardziej różniące się od sibebie.

--- src/window/board/Ball.java
+++ src/window/board/Ball.java
@@ -16,20 +16,20 @@ public class Ball {
 
 
     // grafiki kolorów
-    final static private ImageIcon BLACK = new ImageIcon(new ImageIcon("./img/black.png").getImage().getScaledInstance(SPECIAL_SIZE, SPECIAL_SIZE, Image.SCALE_DEFAULT));
+    final static private ImageIcon BLACK = new ImageIcon(new ImageIcon("./img/black.png").getImage().getScaledInstance(SPECIAL_SIZE, SPECIAL_SIZE, Image.SCALE_SMOOTH));
     final static private ImageIcon WHITE = new ImageIcon(new ImageIcon("./img/white.png").getImage().getScaledInstance(SPECIAL_SIZE, SPECIAL_SIZE, Image.SCALE_DEFAULT));
     final static private ImageIcon RED = new ImageIcon(new ImageIcon("./img/red.png").getImage().getScaledInstance(SIZE, SIZE, Image.SCALE_DEFAULT));
     final static private ImageIcon ORANGE = new ImageIcon(new ImageIcon("./img/orange.png").getImage().getScaledInstance(SIZE, SIZE, Image.SCALE_DEFAULT));
     final static private ImageIcon YELLOW = new ImageIcon(new ImageIcon("./img/yellow.png").getImage().getScaledInstance(SIZE, SIZE, Image.SCALE_DEFAULT));
     final static private ImageIcon GREEN = new ImageIcon(new ImageIcon("./img/green.png").getImage().getScaledInstance(SIZE, SIZE, Image.SCALE_DEFAULT));
-    final static private ImageIcon CYAN = new ImageIcon(new ImageIcon("./img/cyan.png").getImage().getScaledInstance(SIZE, SIZE, Image.SCALE_DEFAULT));
     final static private ImageIcon BLUE = new ImageIcon(new ImageIcon("./img/blue.png").getImage().getScaledInstance(SIZE, SIZE, Image.SCALE_DEFAULT));
     final static private ImageIcon INDIGO = new ImageIcon(new ImageIcon("./img/indigo.png").getImage().getScaledInstance(SIZE, SIZE, Image.SCALE_DEFAULT));
-    final static private ImageIcon pink = new ImageIcon(new ImageIcon("./img/pink.png").getImage().getScaledInstance(SIZE, SIZE, Image.SCALE_DEFAULT));
+    final static private ImageIcon PURPLE = new ImageIcon(new ImageIcon("./img/purple.png").getImage().getScaledInstance(SIZE, SIZE, Image.SCALE_DEFAULT));
+    final static private ImageIcon PINK = new ImageIcon(new ImageIcon("./img/pink.png").getImage().getScaledInstance(SIZE, SIZE, Image.SCALE_DEFAULT));
     
 
     // tablica z kolorami do wyboru
-    final static protected ImageIcon[] colors = {RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, INDIGO, pink};
+    final static protected ImageIcon[] colors = {RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, PURPLE, PINK};
 
 
     // konstruktor (towrzy JLabel zawierający odpowienią grafikę)
